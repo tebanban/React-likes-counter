ef39a5a55b2ea9a345e28692cea669398e9bfb00
tebanban <estebancalf@gmail.com>
2021-03-15 04:54:18 +0000

    second commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-export function Home() {
+export const Home = () => {
+	//initilize a count variable at 0, the setCount function
+	// will be used to re-set the "count" value.
+	const [count, setCount] = useState(0);
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<h2>{count} likes</h2>
+			{/* Reset count to its previous value + 1 */}
+			<span onClick={() => setCount(count + 1)}>👍🏽</span>
+			{/* Reset count to its previous value - 1 */}
+			<span onClick={() => setCount(count - 1)}>👎🏽</span>
+			<div>
+				<h5>Like or dislike to increase/decrease</h5>
+			</div>
 		</div>
 	);
-}
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,21 +1,15 @@
-import React, { useState } from "react";
+//import react into the bundle
+import React from "react";
 import ReactDOM from "react-dom";
+
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
 import "../styles/index.scss";
 
-const Counter = () => {
-	//initilize a count variable at 0, the setCount function
-	// will be used to re-set the "count" value.
-	const [count, setCount] = useState(0);
-	return (
-		<div>
-			<h2>{count} likes</h2>
-			{/* Reset count to its previous value + 1 */}
-			<span onClick={() => setCount(count + 1)}>👍🏽</span>
-			{/* Reset count to its previous value - 1 */}
-			<span onClick={() => setCount(count - 1)}>👎🏽</span>
-			<h3>Like or dislike to increase/decrease</h3>
-		</div>
-	);
-};
+//import your own components
+import { Home } from "./component/home.js";
 
-ReactDOM.render(<Counter />, document.getElementById("app"));
+//render your react application
+ReactDOM.render(<Home />, document.querySelector("#app"));
